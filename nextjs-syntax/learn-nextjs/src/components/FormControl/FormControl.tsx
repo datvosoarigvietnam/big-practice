1a77353f15a8b330b22aa2546dab4bcfe17742f7
Dat Vo <dat.vo@hikoni.com>
2023-10-03 13:53:08 +0700

    Add ConfirmationModal

--- nextjs-syntax/learn-nextjs/src/components/FormControl/FormControl.tsx
+++ nextjs-syntax/learn-nextjs/src/components/FormControl/FormControl.tsx
@@ -1,15 +1,15 @@
+import todoApi from '@/apis/todo.api'
+import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/router'
+import { useMemo, useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
 import InputControl from '../InputControl/InputControl'
 import RadioButton from '../RadioButton'
-import { formatDate } from '../common/constants'
-import { useMutation } from '@tanstack/react-query'
-import { ITodo } from '../TodoList/TodoList'
-import todoApi from '@/apis/todo.api'
 import Spinner from '../Spinner'
-import { toast } from 'react-toastify'
-import { useState, useEffect, useMemo } from 'react'
-import { useRouter } from 'next/router'
-import { error } from 'console'
+import { ITodo } from '../TodoList/TodoList'
+import { formatDate } from '../common/constants'
+import ConfirmationModal from '../ConfirmationModal'
 export interface IForm {
   title: string
   creator: string
@@ -18,19 +18,15 @@ export interface IForm {
   status: string
   id: string
 }
-// const initFormData: IForm = {
-//   title: '',
-//   creator: '',
-//   createAt: '',
-//   desc: '',
-//   status: 'New',
-//   id: '',
-// }
 export const FormControl = ({
   todoDetail,
 }: {
   todoDetail?: ITodo | undefined
 }) => {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
+  // const [isEditModalOpen, setIsEditModalOpen] = useState(false)
+  // const [isAddModalOpen, setIsAddModalOpen] = useState(false)
+
   const defaultValues = useMemo<IForm>(() => {
     const values: IForm = {
       title: '',
@@ -74,6 +70,14 @@ export const FormControl = ({
     // mutationFn: (todo: ITodo, id: string) => todoApi.editTodo(todo, id)
     mutationFn: (todo: ITodo) => todoApi.editTodo(todo),
   })
+  const deleteTodo = (id: string) => {
+    deleteTodoMutation.mutate(id, {
+      onSuccess: () => {
+        toast.success('Delete Todo Success')
+        router.push('/todos')
+      },
+    })
+  }
   const onSubmit = (todo: ITodo) => {
     if (todoDetail?.id) {
       try {
@@ -86,23 +90,29 @@ export const FormControl = ({
         })
       } catch (error) {}
     } else {
-      addTodoMutation.mutate({ ...todo })
+      addTodoMutation.mutate(
+        {
+          title: todo.title,
+          status: todo.status,
+          createAt: todo.createAt,
+          creator: todo.creator,
+          desc: todo.desc,
+        },
+        {
+          onSuccess: () => {
+            toast.success('Add todo is success')
+            router.push('/todos')
+          },
+        },
+      )
     }
-  }
-  const deleteTodo = (id: string) => {
-    deleteTodoMutation.mutate(id, {
-      onSuccess: () => {
-        toast.success('Delete Todo Success')
-        router.push('/todos')
-      },
-    })
   }
 
   return (
     <div>
       {addTodoMutation.isLoading && <Spinner />}
-      {addTodoMutation.isSuccess &&
-        toast.success('Add New Todo Success', { draggablePercent: 60 })}
+      {deleteTodoMutation.isLoading && <Spinner />}
+      {editTodoMutation.isLoading && <Spinner />}
       <h1 className="text-2xl font-bold text-center mb-16">
         {todoDetail?.id ? 'Details Todo' : 'Add New Todo'}
       </h1>
@@ -113,7 +123,6 @@ export const FormControl = ({
             control={control}
             title="Title"
             placeHolder="Enter your title ..."
-            // value={todoDetail?.title}
           />
           {errors.title && (
             <p className="text-red-800">{errors.title.message}</p>
@@ -149,7 +158,6 @@ export const FormControl = ({
             control={control}
             title="Description"
             placeHolder="Descscription Details"
-            // value={todoDetail?.desc}
           />
           {errors.desc && <p className="text-red-800">{errors.desc.message}</p>}
         </div>
@@ -198,7 +206,7 @@ export const FormControl = ({
                 <button
                   type="button"
                   className="bg-[#675BF1] px-10 py-5 text-white font-semibold text-lg rounded-lg cursor-poiter tracking-[2px]"
-                  onClick={() => deleteTodo(todoDetail?.id)}
+                  onClick={() => setIsDeleteModalOpen(true)}
                 >
                   Delete
                 </button>
@@ -207,6 +215,12 @@ export const FormControl = ({
           </div>
         </div>
       </form>
+      <ConfirmationModal
+        isOpen={isDeleteModalOpen}
+        onClose={() => setIsDeleteModalOpen(false)}
+        onConfirm={() => deleteTodo(todoDetail?.id as string)}
+        message="Are you sure you want to delete this item?"
+      />
     </div>
   )
 }
