1200cd034b5272b7ed1cb80331313dbc359f098b
Dat Vo <dat.vo@hikoni.com>
2023-10-05 11:58:45 +0700

    Setup UI for Sidebar

---
+++ big-practice/frontend/src/pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { NextPageWithLayout } from '@/models/common';
+import { MainLayout } from '@/components/layout';
+import Sidebar from '@/components/Sidebar/Sidebar';
+
+const DashBoard: NextPageWithLayout = () => {
+  return (
+    <div className="">
+      <h1>asdasdasdasd</h1>
+    </div>
+  );
+};
+DashBoard.Layout = MainLayout;
+export default DashBoard;
