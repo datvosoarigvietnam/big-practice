a8c92eefa8b6db433f6492e7ed752af2a0132284
Dat Vo <dat.vo@hikoni.com>
2023-10-23 14:03:03 +0700

    Handle error page

---
+++ big-practice/frontend/src/pages/admin/billing/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { NoFeatureLayout } from '@/components/layout/NoFeature';
+import { NextPageWithLayout } from '@/models/common';
+
+const Billing: NextPageWithLayout = () => {
+  return <></>;
+};
+
+Billing.Layout = NoFeatureLayout;
+
+export default Billing;
